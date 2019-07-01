baa1eeb37a1eae9eec4045954f7b104f517b66e4
龚晨印 <314323116@ruc.edu.cn>
2019-07-02 02:06:51 +0800

    Initial commit

---
+++ ID-Filter/IDFilter.py
@@ -0,0 +1,109 @@
+import time
+import pandas as pd
+import os
+import sys
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+import ralib.helper
+import ralib.time
+import ralib.mkfile
+
+current_path = ''
+num_of_region = 0   # 《门店分组表》同龄人，区域种数
+num_of_region2 = 0  # 《员工信息表》有效区域种数
+num_of_store = 0    # 《门店分组表》同龄人，门店种数
+num_of_store2 = 0   # 《员工信息表》有效门店种数
+num_of_worker = 0   # 《员工信息表》所需门店技师总数
+worker_by_group = [{}, {}, {}, {}]  # 用于数据统计
+
+
+# 预处理excel文件
+# path1: 《门店分组表》路径
+# path2: 《员工信息表》路径
+def readfile(path1: str, path2: str) -> pd.DataFrame:
+    # step 1: excel转换成df
+    df1 = pd.read_excel(path1)
+    df2 = pd.read_excel(path2)
+
+    # 所有nan值转换为'-'
+    df1.fillna('-', inplace=True)
+    df2.fillna('-', inplace=True)
+
+    # step 2: df1初步操作
+    df1[["组别", "门店名称", "所属区域"]] = df1[["组别", "门店名称", "所属区域"]].\
+        applymap(ralib.helper.rm_space)  # 去除元素首尾空格
+    df11 = df1[df1["组别"] == "同龄人现在"]        # 选择同龄人组
+    df11 = df11[["门店名称", "所属区域"]]
+    df11.reset_index(drop=True, inplace=True)
+    df11.iloc[7]["门店名称"] = "呼市海亮店"         # 若《门店分组表》更新，检查门店名称命名问题
+
+    # 区域个数（非重复）和门店个数
+    global num_of_region, num_of_store
+    num_of_region, num_of_store = len(df11["所属区域"].unique()), len(df11["门店名称"])
+
+    # step 3: df2初步操作
+    # 去除元素首尾空格
+    df2[["二级部门", "部门", "岗位名称", "入职日期"]] = df2[["二级部门", "部门", "岗位名称", "入职日期"]].\
+        applymap(ralib.helper.rm_space)
+    df22 = df2[df2["岗位名称"].isin(["SPA师", "理疗师", "专家"])]       # 筛选出技师
+    df22 = df22[["二级部门", "部门", "员工号", "岗位名称", "入职日期"]]
+    df22.reset_index(drop=True, inplace=True)
+
+    # step 4: df2保留选定门店
+    df22["区域部门"] = df22["二级部门"] + df22["部门"]
+    df11["区域部门"] = df11["所属区域"] + "区域" + df11["门店名称"]
+    df222 = df22[df22["区域部门"].isin(df11["区域部门"])]
+    df222.reset_index(drop=True, inplace=True)
+
+    # step 5: 插入新变量“群体”
+    df222a = df222.copy()            # avoid SettingWithCopyWarning
+    df222a["群体"] = df222a["入职日期"].map(ralib.time.peer_group)
+    df2222 = df222a[df222a["群体"].isin([1, 2, 3, 4])]
+    global num_of_worker, num_of_region2, num_of_store2
+    num_of_worker = df2222.shape[0]  # 最终被推送员工总人数
+    num_of_region2 = len(df2222["二级部门"].unique())
+    num_of_store2 = len(df2222["部门"].unique())
+    return df2222
+
+
+# step 6b: 在当前目录生成文件夹，并生成csv。data_frame必须为df2的最终版
+def write_csv(data_frame: pd.DataFrame):
+    region = data_frame["二级部门"].unique()
+    group_name = ["0-3组", "4-6组", "7-12组", "12plus"]
+    global current_path
+    current_path = os.getcwd() + os.sep + "csv-files-" + time.strftime("%Y%m%d%H%M%S")
+    current_path = ralib.mkfile.convert_path(current_path)
+    ralib.mkfile.mkdir(current_path)
+    global worker_by_group
+    for r in region:
+        dir_path = current_path + os.sep + r
+        dir_path = ralib.mkfile.convert_path(dir_path)
+        ralib.mkfile.mkdir(dir_path)
+        for g in range(4):
+            result = data_frame[(data_frame["二级部门"] == r) & (data_frame["群体"] == g+1)][["员工号"]]
+            worker_by_group[g][r] = len(result)
+            csv_path = dir_path + os.sep + r + group_name[g] + ".csv"
+            csv_path = ralib.mkfile.convert_path(csv_path)
+            result.T.to_csv(csv_path, index=False, header=0)
+
+
+def statistics():
+    stat = pd.DataFrame(worker_by_group)
+    stat["按组求和"] = stat.apply(lambda x: x.sum(), axis=1)
+    stat.loc["按区域求和"] = stat.apply(lambda x: x.sum())
+    stat.index = ["0-3个月", "4-6个月", "7-12个月", "1年以上", "按区域求和"]
+    file_path = current_path + os.sep + "summary.xlsx"
+    file_path = ralib.mkfile.convert_path(file_path)
+    stat.to_excel(file_path)
+    print("筛选完成！描述文件summary和被推送人员名单已存至 " + current_path)
+    print("数据统计:")
+    print("《门店分组表》有效区域{}个，门店{}家".format(num_of_region, num_of_store))
+    print("《员工信息表》有效区域{}个，门店{}家，技师{}人".format(num_of_region2, num_of_store2, num_of_worker))
+
+
+if __name__ == "__main__":
+    print("开始筛选，请稍等...")
+    df = readfile(sys.argv[1], sys.argv[2])
+    write_csv(df)
+    statistics()
---
+++ ralib/time.py
@@ -0,0 +1,106 @@
+import datetime
+
+
+def last_day(today: datetime.date, n: int) -> datetime.date:
+    """
+    Return the last day of the month n months before today.
+    :param today: date of today
+    :param n: the number of months before today
+    :return: the last day of the month n months before today
+    """
+    if n <= 0:
+        raise ValueError("n must be bigger than 0")
+    first = datetime.date(today.year, today.month, 1)
+    last = datetime.date.max
+    while n > 0:
+        # the last day of the last month
+        last = first - datetime.timedelta(days=1)
+        # the first day of the last month
+        first = datetime.date(last.year, last.month, 1)
+        n -= 1
+    return last
+
+
+def first_day(today: datetime.date, n: int) -> datetime.date:
+    """
+    Return the first day of the month n months before today.
+    :param today: date of today
+    :param n: the number of months before today
+    :return: the first day of the month n months before today
+    """
+    if n <= 0:
+        raise ValueError("n must be bigger than 0")
+    # the last day of the month n months before today
+    last = last_day(today, n)
+    first = datetime.date(last.year, last.month, 1)
+    return first
+
+
+def to_date(date) -> str:
+    """
+    Convert data-time type to string.
+
+    :param date: datetime object
+    :return: string representation of the date
+    """
+    if date == '-':
+        return '-'
+    else:
+        return str(date.date())
+
+
+def month_delta(start_date: str, end_date: str):
+    """
+    Return the month difference of end_date and start_date
+        :param start_date: start date
+        :param end_date: end date
+        :return:  the month difference of the end_date and the start date
+    """
+    if (start_date == '-') or (end_date == '-'):
+        return  # return null to guarantee that sort algorithm could work
+    date1 = datetime.datetime.strptime(start_date, '%Y-%m-%d').date()  # str to date
+    date2 = datetime.datetime.strptime(end_date, '%Y-%m-%d').date()
+    flag = True
+    if date1 > date2:
+        date1, date2 = date2, date1
+        flag = False
+    year_diff = date2.year - date1.year
+    end_month = year_diff * 12 + date2.month
+    delta = end_month - date1.month
+    return -delta if flag is False else delta
+
+
+def peer_group(date_string: str) -> int:
+    """
+    Return the peer group the date belonging to.
+    :param date_string: string format of the date
+    :return: the peer group the date belonging to
+    """
+    if date_string == '-':
+        return 0
+    current = datetime.datetime.strptime(date_string, '%Y-%m-%d').date()  # str to date type
+    today = datetime.date.today()
+    if current >= first_day(today, 3):
+        return 1
+    elif last_day(today, 4) >= current >= first_day(today, 6):
+        return 2
+    elif last_day(today, 7) >= current >= first_day(today, 12):
+        return 3
+    elif last_day(today, 13) >= current:
+        return 4
+
+
+def is_senior(date_string: str) -> bool:
+    """
+    Return True if the date is belonging to senior group, False otherwise.
+    :param date_string: string format of the date
+    :return: True if the date is belonging to senior group, False otherwise
+    """
+    if date_string == '-':  # nan has already been converted to '-'
+        return True  # keep the unknown date because of the analysis requirement of the experiment
+    current = datetime.datetime.strptime(date_string, '%Y-%m-%d').date()  # str转date
+    today = datetime.date.today()
+    if last_day(today, 13) >= current >= datetime.date(2017, 12, 1):
+        return True
+    else:
+        return False
